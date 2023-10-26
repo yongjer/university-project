5e3b6c35179cb36091c0632789d6b578020d40de
yongjer <jerjer920813@gmail.com>
2023-10-26 21:30:04 +0800

    #trust ip

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket
-
+HOST = ''
+PORT = 
 def tcp_communication(outdata):
-    HOST = ''
-    PORT = 7000
+    
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 import socket
 
 HOST = ''
-PORT = 7000
-
+PORT = 
+TRUST_IP = ['']
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-s.listen(1)
+s.listen(len(TRUST_IP))
 
 print(f'server start at: {HOST}:{PORT}')
 print('wait for connection...')
 
 while True:
     conn, addr = s.accept()
+    if addr[0] not in TRUST_IP:
+        print('not trust ip: ' + str(addr))
+        conn.close()
+        break
     print('connected by ' + str(addr))
 
     indata = conn.recv(1024)
