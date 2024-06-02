3efdc9c1c47972ce7fcc012ef5c625550641f7f8
Yong-Jer Chuang <jerjer920813@gmail.com>
2024-06-02 10:25:29 +0800

    feat: Add Gradio app for sentence transformers

---
+++ ai_server/sentence-transformers/app.py
@@ -0,0 +1,3 @@
+import gradio as gr
+
+gr.load("models/sentence-transformers/all-MiniLM-L6-v2").launch()
---
+++ jetson/similarity/app.py
@@ -0,0 +1,16 @@
+from gradio_client import Client
+
+
+client = Client("http://127.0.0.1:7860")
+
+res = client.predict("the man is happy", "the dog is sad\nthe man is excited", api_name="/predict")
+print(res)
+# output the highest similarity index
+
+# Find the maximum number in the list
+max_number = max(res)
+
+# Find the index of the maximum number
+max_index = res.index(max_number)
+
+print(f"The maximum number is {max_number} at index {max_index}")
